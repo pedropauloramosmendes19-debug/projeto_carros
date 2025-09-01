d21ce3f09ff44b99625c54a5c6619c4e2ada1edc
Pedro Mendes <Pedro_M>
2025-09-01 12:28:44 -0300

    alter in the debug section

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ config = Config(REPOSITORY)
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
